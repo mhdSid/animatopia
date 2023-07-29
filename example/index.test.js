2dddf4b73ba0b90c3089e6ae8503414cd71f8419
moeSidani <mohdsidani@gmail.com>
2023-07-30 00:01:58 +0900

    chore: update exmple

--- example/index.test.js
+++ example/index.test.js
@@ -1,6 +1,18 @@
 (async () => {
 	const { matchAnimationFrames } = require('../src')
-		
+	
+	// Animation that runs on page load
+	await matchAnimationFrames({
+		url: 'http://localhost:3000/',
+		selector: '.wrapper',
+		frameRate: 60,
+		maxCaptureDuration: 3000,
+		frameDelay: 20,
+		pageScreenshotDelay: 100,
+		animationName: 'pulse'
+	})
+
+	// Animation that is triggered on user interaction
 	await matchAnimationFrames({
 		url: 'http://localhost:3000/',
 		selector: '.wrapper',
@@ -13,13 +25,27 @@
 			triggerAction: 'click',
 			triggerSelector: '.wrapper'
 		}
-		// cssTransitionData: {
-		//   keyframes: [{offset: 0, transform: 'scale(1)'}, {offset: 1, transform: 'scale(2)'}],
-		//   duration: 1000,
-		//   easing: 'linear',
-		//   iterations: 1,
-		//   fill: '',
-		//   delay: 0
-		// }
+	})
+
+	// CSS transition that is transformed into animation
+	await matchAnimationFrames({
+		url: 'http://localhost:3000/',
+		selector: '.wrapper',
+		frameRate: 60,
+		maxCaptureDuration: 3000,
+		frameDelay: 20,
+		pageScreenshotDelay: 100,
+		cssTransitionData: {
+		  keyframes: [{offset: 0, transform: 'scale(1)'}, {offset: 1, transform: 'scale(2)'}],
+		  duration: 1000,
+		  easing: 'linear',
+		  iterations: 1,
+		  fill: '',
+		  delay: 0
+		},
+		triggerInfo: {
+			triggerAction: 'hover',
+			triggerSelector: '.wrapper'
+		}
 	})
 })()
